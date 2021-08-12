9c29bb9863041dc56b8c45ab4e00a89180e7f0c2
Cole Rayo <cole.rayo@gmail.com>
2021-08-11 17:39:41 -0700

    butchering

--- behw1/config/production.py
+++
@@ -1,9 +0,0 @@
-# Settings that are unique to production go here
-from .base import *  # noqa
-
-DEBUG = False
-
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-
